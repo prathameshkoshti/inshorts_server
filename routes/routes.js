9429aef4a3a2d0c2145456ce77bcb67223be949b
Prathamesh Koshti <prathu48@gmail.com>
2022-03-31 13:03:20 +0530

    routes interchanged

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { setNews } from "../controller/news-controller.js";
 const route = express.Router();
 
-route.get("/news", setNews);
 route.get("/", (req, res) => res.send("Home Page Route"));
+route.get("/news", setNews);
 
 export default route;
