82672db77db82935515d56ae9635587eeb8e68ac
Prathamesh Koshti <prathu48@gmail.com>
2022-04-02 00:14:09 +0530

    fix: config modified

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
-app.use("", Route);
+app.use("/", Route);
 
 const PORT = 8000;
 Connection();
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { setNews } from "../controller/news-controller.js";
 const route = express.Router();
 
-route.get("/", (req, res) => res.send("Home Page Route"));
+route.get("/", (req, res) => res.send("Home Page Route 1"));
 route.get("/news", setNews);
 
 export default route;
